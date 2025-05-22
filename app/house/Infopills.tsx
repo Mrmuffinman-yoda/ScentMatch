bf8089ed6ec006b073b4b61e20c76380c360b556
Harison <harisoncom@gmail.com>
2025-05-22 18:23:29 +0100

    Pill component
    Pill component added which gives simple indicators for each fragrance
    house. Some examples includes expensive,  budget and Niche as quick
    indicators for each house.

---
+++ app/house/Infopills.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const colorClasses: { [key: string]: string } = {
+  p: "badge badge-primary",
+  s: "badge badge-secondary",
+  a: "badge badge-accent",
+  i: "badge badge-info",
+  s: "badge badge-success",
+  w: "badge badge-warning",
+  e: "badge badge-error",
+};
+
+type InfopillsProps = {
+  color: keyof typeof colorClasses;
+  children: React.ReactNode;
+};
+
+const Infopills = ({ color, children }: InfopillsProps) => {
+  return <div className={colorClasses[color]}>{children}</div>;
+};
+
+// Example: specific pills
+export const DesignerPill = () => <Infopills color="p"> Designer </Infopills>;
+
+export const ClonePill = () => <Infopills color="i">Clone</Infopills>;
+
+export const NichePill = () => <Infopills color="a">Niche</Infopills>;
+
+export const BudgetPill = () => <Infopills color="s">Budget</Infopills>;
+
+export const ExpensivePill = () => <Infopills color="e">Expensive</Infopills>;
