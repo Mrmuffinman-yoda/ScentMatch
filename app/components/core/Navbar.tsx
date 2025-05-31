60efda60c0e76ab814b460117e40cb2f23bde18a
Harison <harisoncom@gmail.com>
2025-05-31 20:24:27 +0100

    Updates Navbar to use PageContainer and enhances dropdown menu
    Replaces manual padding in Navbar with PageContainer for improved layout consistency. Adds 'pb' prop to PageContainer for flexible padding control. Enhances Navbar dropdown menu by adding menu items for Profile, Settings, and Logout, improving user interaction and functionality.

--- app/components/core/Navbar.tsx
+++ app/components/core/Navbar.tsx
@@ -1,57 +1,60 @@
 import React from "react";
+import PageContainer from "../general/PageContainer";
 
 const Navbar = () => {
   return (
     <div>
-      <div data-theme="" className="navbar bg-base-100 shadow-sm">
-        <div className="flex-1">
-          <a href="/">
-            <img
-              className="w-20"
-              src="/api/minio/scentmatch/core/logoWithText.png"
-              alt="ScentMatch"
+      <PageContainer pb={2}>
+        <div data-theme="" className="navbar bg-base-100 shadow-sm">
+          <div className="flex-1">
+            <a href="/">
+              <img
+                className="w-20"
+                src="/api/minio/scentmatch/core/logoWithText.png"
+                alt="ScentMatch"
+              />
+            </a>
+          </div>
+          <div className="flex gap-2">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-24 md:w-auto"
             />
-          </a>
-        </div>
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="/api/minio/scentmatch/core/noprofile.png"
-                />
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="/api/minio/scentmatch/core/noprofile.png"
+                  />
+                </div>
               </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
           </div>
         </div>
-      </div>
+      </PageContainer>
     </div>
   );
 };
--- app/components/general/PageContainer.tsx
+++ app/components/general/PageContainer.tsx
@@ -6,12 +6,14 @@ interface Props {
   children?: React.ReactNode;
   maxWidth?: string;
   disableAnim?: boolean;
+  pb?: number;
 }
 
 const PageContainer = ({
   children,
-  maxWidth = "max-w-6xl",
+  maxWidth = "max-w-7xl",
   disableAnim = false,
+  pb = 10,
 }: Props) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { margin: "-100px" });
@@ -27,7 +29,7 @@ const PageContainer = ({
   return (
     <motion.div
       ref={ref}
-      className={`w-full ${maxWidth} mx-auto pb-10`}
+      className={`w-full ${maxWidth} mx-auto pb-${pb}`}
       initial={{ opacity: 0, scale: 0.95 }}
       animate={
         isInView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.95 }
