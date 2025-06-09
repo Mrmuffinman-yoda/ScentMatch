708af9103c21d489c9b12e8ab901709b4b6982cd
Harison <harisoncom@gmail.com>
2025-06-09 14:57:14 +0100

    Replaces dropdown with login link in Navbar
    Updates the Navbar component to replace the user avatar dropdown menu with a login link. Adds a TODO note to implement the dropdown for logged-in users in the future, simplifying the current structure.

--- app/components/core/Navbar.tsx
+++ app/components/core/Navbar.tsx
@@ -21,12 +21,9 @@ const Navbar = () => {
               placeholder="Search"
               className="input input-bordered w-24 md:w-auto"
             />
-            <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar"
-              >
+            {/* TODO: once the user is logged in, change this button to a drop down instead of routing to the login page */}
+            <Link href="/login">
+              <div className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
                   <img
                     alt="Tailwind CSS Navbar component"
@@ -34,24 +31,7 @@ const Navbar = () => {
                   />
                 </div>
               </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-              >
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
